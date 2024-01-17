54a3cfce9ee25323130431350833de03a91e0d08
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-01-18 00:07:58 +0600

    Error handlr added on root

--- dist/server.js
+++ dist/server.js
@@ -5,13 +5,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const root_routes_1 = require("./routes/root.routes");
 const port = process.env.PORT || 5000;
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+// middleware
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 // routes
 app.use("/api/v1", root_routes_1.rootRoutes);
+// 404 route not found
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Not Found",
+        error: "API endpoint you hit is not found",
+    });
+    next();
+});
+// global error handler
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+        error: error,
+    });
+});
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,38 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { rootRoutes } from "./routes/root.routes";
 const port = process.env.PORT || 5000;
 dotenv.config();
 
 const app: Application = express();
 
+// middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api/v1", rootRoutes);
+
+// 404 route not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Not Found",
+    error: "API endpoint you hit is not found",
+  });
+  next();
+});
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+    error: error,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
